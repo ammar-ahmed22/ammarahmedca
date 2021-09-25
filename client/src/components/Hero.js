ea336915ef59292721f3b3b5dd8c6067388e3a55
ammar-ahmed22 <ammar.ahmed2203@gmail.com>
2021-09-25 03:18:54 -0400

    Added animations to hero with react-spring

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
+import Animated from './Animated';
 import '../assets/css/hero.css'
+
 
 //import canvasHover from '../assets/images/canvashover.png';
 
@@ -46,17 +48,28 @@ class Hero extends Component {
             return <Redirect push to="/snake"/>;
         }
         console.log(this.state.canvasId)
+
+        
         return (
             <section className="hero d-flex align-items-center" id="home">
                 <div className="container">
                 <div className="row ">
                     <DarkModeToggle isDark={this.props.isDark} toggleIsDark={this.props.toggleIsDark} updateCanvasId={this.updateCanvasId}/>
                     <div className="col-md-6  d-flex flex-column">
-                        <Typist avgTypingDelay={120} cursor={{show: false}}>
-                            <h1 className="display-1 text-light type-text">hi, i'm <span className="text-purple">ammar</span></h1>
-                        </Typist>
-                        <h2 className="text-light">Nanotechnology Engineer with a passion to learn</h2>
-                        <p className="text-slate fs-5">I’m a second year engineering student at the University of Waterloo with a strong interest in software development</p>
+                        <Animated animProps={{to: {opacity: 1, marginTop: 0}, from: {opacity: 0, marginTop: 200}, config: {duration: 2000}}}>
+                            <Typist avgTypingDelay={120} cursor={{show: false}}>
+                                <h1 className="display-1 text-light type-text">hi, i'm <span className="text-purple">ammar</span></h1>
+                            </Typist>    
+                        </Animated>
+                        <Animated animProps={{to: {opacity: 1, marginTop: 0}, from: {opacity: 0, marginTop: 200}, config: {duration: 2000}}}>
+                            <h2 className="text-light">Nanotechnology Engineer with a passion to learn</h2>
+                        </Animated>
+                        
+                        {/* <h2 className="text-light">Nanotechnology Engineer with a passion to learn</h2> */}
+                        <Animated animProps={{to: {opacity: 1, marginTop: 0}, from: {opacity: 0, marginTop: 200}, config: {duration: 2000}}}>
+                            <p className="text-slate fs-5">I’m a second year engineering student at the University of Waterloo with a strong interest in software development</p>
+                        </Animated>
+                        
                         
                         <a href="#about" className="scroll-ind text-purple text-decoration-none display-5 cursor-pointer"><i class='bx bxs-down-arrow-alt'></i></a>
                         
@@ -66,7 +79,12 @@ class Hero extends Component {
                 </div>
                 
 
-                {!this.mediaQuery.matches && <RobotModel isDark={this.props.isDark} id={this.state.canvasId} />}
+                {!this.mediaQuery.matches && (
+                <Animated animProps={{to: {opacity: 1}, from: {opacity: 0}, config: {duration: 2000}}}>
+                    <RobotModel isDark={this.props.isDark} id={this.state.canvasId} />
+                </Animated>
+                
+                )}
        
       </section>
         );
