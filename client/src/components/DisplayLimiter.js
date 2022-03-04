ddbd2c2840f82f8b83ba89e0b5489f8650dd0344
ammar-ahmed22 <ammar.ahmed2203@gmail.com>
2022-03-04 02:25:34 -0500

    display limiter complete

---
+++ client/src/components/DisplayLimiter.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Button, Text } from "@chakra-ui/react"
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
+
+const DisplayLimiter = ({ numDisplaying, setNumDisplaying, initial, total, incrementBy, scrollToId=false}) => {
+
+    const handleShowMore = e => {
+        if (numDisplaying + incrementBy < total){
+            setNumDisplaying( prev => prev + incrementBy)
+        }else{
+            setNumDisplaying(total)
+        }
+    }
+
+    const handleShowLess = e => {
+        setNumDisplaying(initial)
+        if (scrollToId){
+            document.getElementById(scrollToId).scrollIntoView({
+                behavior: "smooth"
+            })
+        }
+    }
+
+    const styleProps = {
+        main: {
+            variant: "ghost",
+            display: "flex",
+            flexDirection: "column",
+            _hover: {
+                color: "primaryLight"
+            },
+            _focus: {}
+        }
+    }
+
+    if (numDisplaying < total){
+        return (
+            <Button {...styleProps.main} onClick={handleShowMore}><Text>Show more</Text><ChevronDownIcon /></Button>
+        );
+    }else if (numDisplaying === total){
+        return (
+            <Button {...styleProps.main} onClick={handleShowLess}><Text>Show less</Text><ChevronUpIcon /></Button>
+        )
+    }
+    
+}
+
+export default DisplayLimiter;
