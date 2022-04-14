ec50926d0585586d57c5121fddea190e3f5c285b
Ammar Ahmed <ammar@Ammars-MacBook-Pro.local>
2022-04-14 00:25:06 -0400

    AA-7 game is rendering

--- client/src/pages/Chess.js
+++ client/src/pages/Chess.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../components/NavBar';
 import PageContent from '../components/PageContent';
 import Footer from '../components/Footer';
-import { Text } from "@chakra-ui/react"
+import Board from '../components/Chess/Board';
+import { Text, Box, Flex } from "@chakra-ui/react"
 
 const Chess = () => {
+
+    const [fen, setFen] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
     return (
         <>
             <NavBar active="chess"/>
             <PageContent>
-                <Text>Chess game bro</Text>
+                <Flex justify="center" align="center" mt="10vh">
+                    <Board size={400} fen={fen} setFen={setFen}/>
+                </Flex>
             </PageContent>
             <Footer />
         </>
