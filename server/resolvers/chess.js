1e377f14943232e728b2658a06c715da285f4d98
Ammar Ahmed <ammar@ammar.local>
2022-04-21 01:50:11 -0400

    queries for opponents created

--- server/resolvers/chess.js
+++ server/resolvers/chess.js
@@ -1,24 +1,51 @@
 const Opponent = require("../models/Opponent");
 const Game = require("../models/Game");
+const { UserInputError } = require("apollo-server-express")
 
 const resolvers = {
     Query: {
-        getOpponent: () => {
-            return {
-              id: "1",
-              name: {
-                first: "Yasir",
-                middle: "Pathan",
-                last: "Ahmed"
-              },
-              email: "yasirahmed@gmail.com"
+        getAllOpponents: async () => {
+            const opps = await Opponent.find({});
+
+            if (opps.length){
+                return opps;
+            }else{
+                throw new UserInputError("No opponents found")
             }
         },
+        getOpponentByEmail: async (_, { email }) => {
+            const opps = await Opponent.find({ email });
+
+            if (opps.length){
+                return opps[0]
+            }else{
+                throw new UserInputError("No opponents found with email", { email })
+            }
+        },
+        getOpponentById: async (_, { id }) => {
+            const opp = await Opponent.findById(id);
+
+            if (opp){
+                return opp
+            }else{
+                throw new UserInputError("No opponent found with id", { id })
+            }
+        }
     },
     Mutation: {
         createOpponent: async (_, { firstName, lastName, middleName, email }) => {
 
-            console.log(firstName, lastName, middleName, email)
+            //console.log(firstName, lastName, middleName, email)
+
+            const existingOpp = await Opponent.find({ email })
+
+            if (existingOpp.length){
+                console.log(`OPPONENT WITH EMAIL: ${email} ALREADY EXISTS`)
+                console.log(existingOpp)
+                throw new UserInputError("Opponent with email already exists", {
+                    email
+                })
+            }
 
             const name = {
                 first: firstName,
@@ -51,28 +78,13 @@ const resolvers = {
                 }
             }
 
-            // console.log(game)
-            // console.log(opp)
-
-            //const { __doc } = game;
-
-            const result = {...game}._doc
             
-            delete result.__v;
-            delete result.createdAt;
-            delete result.updatedAt;
             
-            console.log(result)
+            
+            
 
-            return result
-            return {
-                id: "1",
-                oppID: "1",
-                moves: [{ fen: "openingFEN", playedAt: "now"}],
-                oppToMove: true,
-                oppWon: false,
-                tied: false
-            }
+            return game
+            
         }
     }
     
