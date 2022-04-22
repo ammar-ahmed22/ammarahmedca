a8eb23e3b98965738905bd5da8b826a645815bc8
Ammar Ahmed <ammar@ammar.local>
2022-04-22 19:13:13 -0400

    added methods and fields to Opponent

--- server/src/models/Opponent.js
+++ server/src/models/Opponent.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import { runInThisContext } from "vm";
 
 const OpponentSchema = new mongoose.Schema({
     name: {
@@ -6,11 +10,45 @@ const OpponentSchema = new mongoose.Schema({
         last: { type: String, required: true },
         middle: String
     },
-    email: {type: String, required: true},
+    email: {type: String, required: [true, "Please provide an e-mail"], unique: true, match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "Please provide a valid e-mail"]},
+    password: {type: String, required: [true, "Please provide a password"], minlength: 6, select: false},
     signedupAt: { type: Date, required: true},
+    permissions: { type: Array},
     currentGameID: String,
+    resetPasswordExpire: Date,
+    resetPasswordToken: String,
     gameHistory: [{ gameID: String, won: Boolean, tie: Boolean}],
 }, { timestamps: true })
+
+// Called before saved to DB
+// Checks if password was modified and hashes it 
+OpponentSchema.pre("save", async function (next){
+    if (!this.isModified("password")){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+
+    this.password = await bcrypt.hash(this.password, salt);
+
+})
+
+// Checks password on login
+OpponentSchema.methods.matchPasswords = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+//Provides signed JWT
+OpponentSchema.methods.getSignedJWT = function(){
+    return jwt.sign({ id: this._id, permissions: this.permissions }, process.env.JWT_SECRET)
+}
+
+OpponentSchema.methods.getResetPasswordToken = function(){
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    this.resetPasswordToken = crypto.crypto.createHash("sha256").update(resetToken).digest("hex");
+    this.resetPasswordExpire = Date.now() + 10 * (60 * 1000) // 10 mins
+
+}
 
 const Opponent = mongoose.model("Opponent", OpponentSchema);
 
