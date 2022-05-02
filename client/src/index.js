c5208a7ae25fbf6d8f81c4421bb2448a3c0b9a9e
Ammar Ahmed <ammar@ammar.local>
2022-05-01 23:53:13 -0400

    workflow updates

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { ChakraProvider, extendTheme, ColorModeScript } from "@chakra-ui/react"
-
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { fab } from "@fortawesome/free-brands-svg-icons"
-
 import { CookiesProvider } from "react-cookie"
-
 import { faFileDownload, faCircle, faExternalLinkAlt, faSearch, faEnvelope, faHome, faUser, faBriefcase, faFileAlt, faPen, faMouse, faChessPawn } from "@fortawesome/free-solid-svg-icons"
 
 library.add(fab, faFileDownload, faCircle, faExternalLinkAlt, faSearch, faEnvelope, faHome, faUser, faBriefcase, faFileAlt, faPen, faMouse, faChessPawn)
