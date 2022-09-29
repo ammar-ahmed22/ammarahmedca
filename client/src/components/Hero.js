254a61f39e7f92661feaa693b9a394cf7d26edc6
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2022-09-29 04:01:38 -0400

    updated current position

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -33,7 +33,7 @@ const Hero = () => {
             <Box marginTop={"10vh"} zIndex={10} >
                 <Text {...styleProps.mainText} as="h1">Hello <Text as="span" fontSize={{ base: "5xl", lg: "7xl"}}>👋🏽</Text><br />I'm <Text color={useColorModeValue("primaryLight", "primaryDark")} as="span">Ammar</Text></Text>
                 <Text {...styleProps.subText}>Engineering student <Text as="span" fontWeight="bold">@</Text> <Link href="https://uwaterloo.ca/" {...styleProps.subTextLink}>University of Waterloo</Link></Text>
-                <Text {...styleProps.subText}>Data Analyst <Text as="span" fontWeight="bold">@</Text> <Link href="https://fragrancebuy.ca/" {...styleProps.subTextLink}>FragranceBuy</Link></Text>
+                <Text {...styleProps.subText}>Frontend Developer <Text as="span" fontWeight="bold">@</Text> <Link href="https://docs.aiarena.io/" {...styleProps.subTextLink}>AI Arena</Link></Text>
             </Box>
             <Signature color={useColorModeValue("black", "white")} />
             
