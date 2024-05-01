4d1d351f180b5abb068c223c61c23b732c42e0cb
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-05-01 11:39:59 -0400

    feat: removed ALL old code

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-/// <reference path="./chess/types/game.d.ts"/>
-/// <reference path="./chess/types/context.d.ts"/>
-/// <reference path="./chess/types/components.d.ts"/>
-/// <reference path="./chess/types/api.d.ts"/>
-
-declare module "react-syntax-highlighter";
-declare module "react-syntax-highlighter/dist/esm/styles/hljs";
