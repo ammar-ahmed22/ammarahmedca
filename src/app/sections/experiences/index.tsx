b22da47ba11298dee20500bf76442e2533138bce
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-19 13:10:06 -0500

    feat(client): Experiences timeline complete

--- src/app/sections/experiences/index.tsx
+++ src/app/sections/experiences/index.tsx
@@ -1,4 +1,7 @@
 import api from "@/lib/api";
+import Timeline from "@/components/ui/timeline";
+import { formatDateRange } from "@/lib/date";
+import { Badge } from "@/components/ui/badge";
 
 export const revalidate = 60;
 
@@ -13,14 +16,41 @@ export default async function Experiences() {
       <p className="text-neutral-500 text-xl mb-4">
         See where I&apos;ve been working!
       </p>
-      {experiences.map((experience) => {
-        return (
-          <div key={experience.id} className="mb-2">
-            <p className="text-xl">{experience.company}</p>
-            <p className="text-neutral-500">{experience.role}</p>
-          </div>
-        );
-      })}
+      <Timeline
+        data={experiences.map((experience) => {
+          return {
+            title: experience.company,
+            subtitle: experience.timeframe
+              ? formatDateRange(experience.timeframe, "MMM yyyy")
+              : undefined,
+            icon: experience.icon,
+            content: (
+              <div className="flex flex-col gap-2">
+                <small className="uppercase text-sm font-bold">
+                  {experience.type}
+                </small>
+                <p className="text-lg">{experience.role}</p>
+                <p className="text-neutral-500">
+                  {experience.description
+                    .map((r) => r.plainText)
+                    .join("")}
+                </p>
+                <div className="flex flex-wrap gap-2">
+                  {experience.skills.map((skill) => {
+                    return (
+                      <Badge
+                        key={`${experience.id}-${skill}`}
+                        variant="outline">
+                        {skill}
+                      </Badge>
+                    );
+                  })}
+                </div>
+              </div>
+            ),
+          };
+        })}
+      />
     </section>
   );
 }
