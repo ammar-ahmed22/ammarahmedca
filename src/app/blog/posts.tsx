596c7e16fbb4748f8cc642c60aaea2c28cc6aa2c
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-21 20:58:15 -0500

    feat(client): Add searching for blog posts

--- src/app/blog/posts.tsx
+++ src/app/blog/posts.tsx
@@ -1,19 +1,52 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { PostMetadata } from "@/types/api";
 import RichText from "@/components/ui/rich-text";
 import { Card, CardContent } from "@/components/ui/card";
 import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
+import { Input } from "@/components/ui/input";
+import { SearchIcon, FrownIcon } from "lucide-react";
 
 export type PostsProps = {
   posts: PostMetadata[];
 };
 
 export default function Posts({ posts }: PostsProps) {
+  const [query, setQuery] = useState("");
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  useEffect(() => {
+    setFilteredPosts(
+      posts.filter((post) => {
+        const lowerCaseQuery = query.toLowerCase();
+        return (
+          post.name.toLowerCase().includes(lowerCaseQuery) ||
+          post.category?.toLowerCase().includes(lowerCaseQuery) ||
+          post.tags.some((tag) =>
+            tag.toLowerCase().includes(lowerCaseQuery),
+          )
+        );
+      }),
+    );
+  }, [posts, query]);
+
   return (
     <div className="flex flex-col gap-8 w-full">
-      {posts.map((post, idx, all) => {
+      <Input
+        className="w-full"
+        startIcon={SearchIcon}
+        placeholder="Search by title, category or tag"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {filteredPosts.length === 0 && (
+        <div className="flex flex-col items-center justify-center h-96">
+          <FrownIcon className="size-10" />
+          <p className="font-bold text-xl">Nothing found</p>
+        </div>
+      )}
+      {filteredPosts.map((post, idx, all) => {
         const isLast = idx === all.length - 1;
         return (
           <React.Fragment key={post.id}>
