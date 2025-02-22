24272e475a89831a846021fd9c5eb63559da13e4
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-22 14:07:22 -0500

    feat(client): Blog posts are now clickable with link to post page

--- src/app/blog/posts.tsx
+++ src/app/blog/posts.tsx
@@ -7,6 +7,7 @@ import { format } from "date-fns";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { SearchIcon, FrownIcon } from "lucide-react";
+import Link from "next/link";
 
 export type PostsProps = {
   posts: PostMetadata[];
@@ -50,61 +51,65 @@ export default function Posts({ posts }: PostsProps) {
         const isLast = idx === all.length - 1;
         return (
           <React.Fragment key={post.id}>
-            <Card
-              key={post.id}
-              className="w-full flex-grow border-none hover:bg-neutral-600/50 [&_img]:hover:blur-sm cursor-pointer transition-all ease-in-out duration-200 relative">
-              {post.image && post.category && (
-                <Badge
-                  className="absolute top-4 left-4 z-20 rounded-full border-foreground/30 bg-background/60"
-                  variant="outline">
-                  {post.category}
-                </Badge>
-              )}
-              {post.image && (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={post.image}
-                  alt={post.name + " cover image"}
-                  className="w-full h-96 object-cover rounded-t-xl mb-4"
-                />
-              )}
-              <CardContent>
-                {!post.image && post.category && (
-                  <div className="my-4">
-                    <Badge
-                      className="rounded-full border-foreground/30 bg-background/60"
-                      variant="outline">
-                      {post.category}
-                    </Badge>
+            <Link href={`/blog/${post.slug}`}>
+              <Card
+                key={post.id}
+                className="w-full shadow-none flex-grow border-none hover:bg-neutral-300/50 dark:hover:bg-neutral-600/50 [&_img]:hover:scale-125 [&_img]:scale-100 cursor-pointer transition-all ease-in-out duration-200 relative">
+                {post.image && post.category && (
+                  <Badge
+                    className="absolute top-4 left-4 z-20 rounded-full border-foreground/30 bg-background/60"
+                    variant="outline">
+                    {post.category}
+                  </Badge>
+                )}
+                {post.image && (
+                  <div className="w-full overflow-hidden mb-4">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={post.image}
+                      alt={post.name + " cover image"}
+                      className="w-full h-96 object-cover rounded-t-xl transition-transform duration-200 ease-in"
+                    />
                   </div>
                 )}
-                <span className="text-neutral">
-                  {post.date && format(post.date, "MMM dd, yyyy")}
-                  {/* TODO (potential): Add read time */}
-                  {/* • 10 min read */}
-                </span>
-                <h4 className="text-xl font-bold mt-2">
-                  {post.name}
-                </h4>
-                <RichText
-                  as="p"
-                  data={post.description}
-                  className="mb-4"
-                />
-                <div className="flex flex-wrap gap-2">
-                  {post.tags.map((tag, tagIdx) => {
-                    return (
+                <CardContent>
+                  {!post.image && post.category && (
+                    <div className="my-4">
                       <Badge
-                        key={`post-${post.id}-tag-${tagIdx}`}
-                        variant="outline"
-                        className="border-foreground/30">
-                        #{tag}
+                        className="rounded-full border-foreground/30 bg-background/60"
+                        variant="outline">
+                        {post.category}
                       </Badge>
-                    );
-                  })}
-                </div>
-              </CardContent>
-            </Card>
+                    </div>
+                  )}
+                  <span className="text-neutral">
+                    {post.date && format(post.date, "MMM dd, yyyy")}
+                    {/* TODO (potential): Add read time */}
+                    {/* • 10 min read */}
+                  </span>
+                  <h4 className="text-xl font-bold mt-2">
+                    {post.name}
+                  </h4>
+                  <RichText
+                    as="p"
+                    data={post.description}
+                    className="mb-4"
+                  />
+                  <div className="flex flex-wrap gap-2">
+                    {post.tags.map((tag, tagIdx) => {
+                      return (
+                        <Badge
+                          key={`post-${post.id}-tag-${tagIdx}`}
+                          variant="outline"
+                          className="border-foreground/30">
+                          #{tag}
+                        </Badge>
+                      );
+                    })}
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
             {!isLast && (
               <hr className="w-full border-neutral-500/30" />
             )}
