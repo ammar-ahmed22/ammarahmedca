cc545b066e1fd90ca8f6da18d56b1fc1e98ae442
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-21 19:01:31 -0500

    feat(client): Blog posts page with cards

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,23 @@
+import api from "@/lib/api";
 import { Metadata } from "next";
+import Posts from "./posts";
 
 export const metadata: Metadata = {
   title: "Blog",
 };
 
-export default function Blog() {
-  return <div>Blog page</div>;
+export default async function Blog() {
+  const posts = await api.posts.list({ onlyPublished: true });
+  return (
+    <div className="flex flex-col gap-4 items-center">
+      <div className="md:w-3/5">
+        <h1 className="text-4xl font-bold text-center">Blog</h1>
+        <p className="text-lg text-neutral text-center">
+          Sometimes I like to write about things I&apos;ve worked on,
+          my experiences, or anything else of interest to me.
+        </p>
+      </div>
+      <Posts posts={posts} />
+    </div>
+  );
 }
