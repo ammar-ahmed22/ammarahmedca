5a8174b57b7fd3e3470954db75539e47be763dde
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-21 21:00:03 -0500

    fix(client): Blog heading font family

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,8 +10,10 @@ export default async function Blog() {
   const posts = await api.posts.list({ onlyPublished: true });
   return (
     <div className="flex flex-col gap-4 items-center">
-      <div className="md:w-3/5">
-        <h1 className="text-4xl font-bold text-center">Blog</h1>
+      <div className="md:w-3/5 w-full mb-8">
+        <h1 className="text-4xl font-display font-bold text-center">
+          Blog
+        </h1>
         <p className="text-lg text-neutral text-center">
           Sometimes I like to write about things I&apos;ve worked on,
           my experiences, or anything else of interest to me.
