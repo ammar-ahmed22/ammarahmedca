c892fb75f8216726c00525adb94333bf1a28833e
Ammar Ahmed <ammar.ahmed@withmantle.com>
2025-02-25 17:38:26 -0500

    feat(client): About page fetches specific about blog post to render

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,28 @@
 import { Metadata } from "next";
+import api from "@/lib/api";
+import Block from "../blog/[slug]/block";
 
-export const metadata: Metadata = {
-  title: "About",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const [post] = await api.posts.list({ slug: "about" });
+  return {
+    title: "About",
+    description: post.description.map((r) => r.plainText).join(""),
+    openGraph: {
+      title: "About",
+      description: post.description.map((r) => r.plainText).join(""),
+      type: "website",
+      siteName: "ammarahmed.ca",
+    },
+  };
+}
 
-export default function About() {
-  return <div>About page</div>;
+export default async function About() {
+  const { content } = await api.posts.content({ slug: "about" });
+  return (
+    <div className="w-full flex flex-col gap-4">
+      {content.map((block) => {
+        return <Block key={block.id} block={block} />;
+      })}
+    </div>
+  );
 }
