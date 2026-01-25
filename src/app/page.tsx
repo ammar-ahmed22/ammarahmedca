6322004a68aaadf7de8e5138ce6e4ffb13935b2c
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2026-01-25 01:05:16 -0500

    feat: re-order experience first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Projects />
       <Experiences />
+      <Projects />
     </>
   );
 }
--- src/app/sections/experiences/index.tsx
+++ src/app/sections/experiences/index.tsx
@@ -11,7 +11,7 @@ export default async function Experiences() {
   const experiences = await api.experiences.list();
 
   return (
-    <section className="min-h-screen" id="experience">
+    <section className="min-h-screen mb-24" id="experience">
       <h2 className="text-4xl mb-1 font-display font-bold">
         Experiences
       </h2>
