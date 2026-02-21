2e5a588cb0e2dd657fb5be14ea5b0a1499e1a29b
Ammar Ahmed <ammar.ahmed2203@gmail.com>
2026-02-21 01:32:58 -0500

    feat: hero section updates

--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -1,15 +1,8 @@
 "use client";
 import React from "react";
 import Signature from "@/components/ui/signature";
-import {
-  ArrowDownIcon,
-  HammerIcon,
-  BabyIcon,
-  GemIcon,
-} from "lucide-react";
-import { PiMosqueBold } from "react-icons/pi";
+import { ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
 
 export type QuickLink = {
   href: string;
@@ -17,47 +10,11 @@ export type QuickLink = {
   onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void;
 };
 
-export type BadgeContent = {
-  icon: React.ReactNode;
-  label: string;
-};
-
 export default function Hero() {
-  const badges: BadgeContent[] = [
-    {
-      icon: <HammerIcon className="size-3" />,
-      label: "Engineer",
-    },
-    {
-      icon: <PiMosqueBold className="size-3" />,
-      label: "Muslim",
-    },
-    {
-      icon: <GemIcon className="size-3" />,
-      label: "Husband",
-    },
-    {
-      icon: <BabyIcon className="size-3" />,
-      label: "Father",
-    },
-  ];
   return (
     <section className="flex flex-col min-h-screen pt-[10vh]">
       <header className="flex flex-col">
-        <div className="flex gap-2 items-center mb-2">
-          {badges.map((badge, index) => {
-            return (
-              <Badge
-                key={index}
-                className="w-fit rounded-full border-primary/30 flex items-center gap-2"
-                variant="outline"
-              >
-                {badge.icon} {badge.label}
-              </Badge>
-            );
-          })}
-        </div>
-        <div className="flex gap-4 sm:text-7xl text-5xl font-display items-center flex-wrap font-bold z-20 mb-4">
+        <div className="flex gap-4 sm:text-7xl text-5xl font-display items-center flex-wrap font-bold z-20">
           <span>Hi, I&apos;m</span>
           <div className="flex items-center gap-3">
             <div className="border border-foreground rounded-2xl p-1 relative">
@@ -72,6 +29,23 @@ export default function Hero() {
             </div>
             <span>Ammar!</span>
           </div>
+        </div>
+        <div className="font-body sm:text-xl text-lg text-neutral font-bold">
+          Muslim. Engineer. Husband. Father.
+        </div>
+        <div className="font-body sm:text-lg text-base text-neutral">
+          I build software and write about my thoughts and
+          experiences.
+        </div>
+        <div className="font-body sm:text-lg text-base text-neutral">
+          Currently at the{" "}
+          <a
+            href="https://uwaterloo.ca"
+            className="underline underline-offset-4"
+          >
+            University of Waterloo
+          </a>
+          .
         </div>
         <div className="flex justify-start pt-3 mb-3">
           <Signature className="h-[40vh]" duration={1} />
